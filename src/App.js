64e1568e7f1be23b8969cda234d6f95594982228
vienpham268 <vienpham268@gmail.com>
2020-04-25 23:38:46 +0700

    Added navbar-route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Movies from './components/movies'
+import Customers from './components/customers';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import NavBar from './components/commons/navBar'
+import MovieForm from './components/movieForm';
+import './App.css';
+
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <main className='container'>
-       
-        <Movies></Movies>
-      </main>
-     );
+  state = {}
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className='container'>
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" exact component={Customers}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Route path="/rentals" exact component={Rentals}></Route>
+            <Redirect from="/" exact to="/movies"></Redirect>
+            <Redirect to="/not-found"></Redirect>
+          </Switch>
+
+        </main>
+
+      </React.Fragment>
+
+    );
   }
 }
- 
+
 export default App;
 
--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import Like from './commons/like'
+import {Link} from 'react-router-dom'
 import Table from './commons/table'
+import Like from './commons/like'
 class MoviesTable extends Component {
     columns = [
-        { path: 'title', label: 'Title' },
+    { path: 'title', label: 'Title', content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
         { path: 'genre.name', label: 'Genre' },
         { path: 'numberInStock', label: 'Stock' },
         { path: 'dailyRentalRate', label: 'Rate' },
