350e9d83d69d53cdd58a4c068ccfbdcbd854869c
vienpham <vien.pham@pexraytech.com>
2020-04-26 09:48:16 +0700

    added loginForm component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom'
-import Movies from './components/movies'
-import Customers from './components/customers';
-import NotFound from './components/notFound';
-import Rentals from './components/rentals';
-import NavBar from './components/commons/navBar'
-import MovieForm from './components/movieForm';
-import './App.css';
-
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
+import Rentals from "./components/rentals";
+import NavBar from "./components/commons/navBar";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 class App extends Component {
-  state = {}
+  state = {};
   render() {
     return (
       <React.Fragment>
         <NavBar />
-        <main className='container'>
+        <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm}></Route>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" exact component={Customers}></Route>
@@ -25,14 +26,10 @@ class App extends Component {
             <Redirect from="/" exact to="/movies"></Redirect>
             <Redirect to="/not-found"></Redirect>
           </Switch>
-
         </main>
-
       </React.Fragment>
-
     );
   }
 }
 
 export default App;
-
