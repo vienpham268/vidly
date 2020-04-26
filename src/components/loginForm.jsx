392f1110eae26afc47218b505a249ede68140b4f
vienpham268 <vienpham268@gmail.com>
2020-04-27 00:20:37 +0700

    added joi-brower for validating form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,23 +1,76 @@
 import React, { Component } from "react";
-
+import Joi from "joi-browser";
+import Input from "./commons/input";
 class LoginForm extends Component {
-  handleSumit = (e) => {
+  state = {
+    account: { username: "", password: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+    const errors = {};
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
+    }
+
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submitted')
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    // call server
+    console.log("Form submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account, errors });
   };
   render() {
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form onSubmit={this.handleSumit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            onChange={this.handleChange}
+            name="username"
+            value={account.username}
+            label="UserName"
+            error={errors.username}
+          />
+          <Input
+            onChange={this.handleChange}
+            name="password"
+            value={account.password}
+            label="Password"
+            error={errors.password}
+          />
+
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
